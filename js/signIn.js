98edac625afd180ba8ce0da755ceb4d31080ec5e
farhan <onihilinterit@gmail.com>
2025-04-29 19:17:04 +0700

    first commit

---
+++ js/signIn.js
@@ -0,0 +1,26 @@
+// file ini untuk penghubung antara UI HTML dan model User
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const userForm = document.getElementById('userForm');
+    const userManager = new User();
+
+    userForm.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+
+        const usernameByInput = document.getElementById('username').value;
+
+        const result = userManager.signInUser(usernameByInput);
+
+        if(result.success){
+            localStorage.setItem('usernameLoggedIn', usernameByInput);
+            return window.location.href = '../tasks.html';
+        }
+        else {
+            alert('Username tidak ditemukan');
+            console.log(result.message);
+        }
+    });
+
+});
